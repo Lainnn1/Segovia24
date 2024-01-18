875a50a7c4eefc0574cbeacfc73812990f829a4f
Lainnn1 <calvolain8@gmail.com>
2024-01-18 14:57:46 +0100

    commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #this is my first readme 
+print(21//7)
+
--- session3-4.py
+++ session3-4.py
@@ -1,4 +1,10 @@
-print("hello vukasin")
-print("this is a new change")
-print("this is a new change")
-print("this is a new change")
+print("hello world")
+
+a= 5
+b= 4
+print( a is b)
+
+print(10%5)
+print(10%9)
+
+
