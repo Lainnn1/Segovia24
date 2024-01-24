f1bb00e059c3df35c64e003dad4c0443a69380c5
Lainnn1 <calvolain8@gmail.com>
2024-01-24 16:13:18 +0100

    commit

---
+++ session6-7.py
@@ -0,0 +1,13 @@
+#naming variables
+a=6
+print(a+2.0)
+a/=2
+print(a)
+b=3
+print(a**b)
+b-= a
+prompt = 'What is the speed of the car'
+speed = input(prompt)
+#What is the speed of the car ?
+int(speed)
+
