575263509d116af2db349632b505926fb75cb624
Lainnn1 <calvolain8@gmail.com>
2024-01-31 17:33:21 +0100

    commit

---
+++ methods.py
@@ -0,0 +1,16 @@
+#a method is an operation
+
+text = "abcdefg"
+print(dir(text))
+(help(text.isidentifier))
+print("1234".isidentifier())
+
+s = "Hello world my name is Lain"
+
+print(s.capitalize())
+
+print(s.capitalize())
+
+print(s.split(" "))
+
+
