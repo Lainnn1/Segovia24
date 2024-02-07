2c4f3c107a544c7fd643db22145d3377fc2b8b66
Lainnn1 <calvolain8@gmail.com>
2024-02-07 16:17:10 +0100

    commit

---
+++ ex.py
@@ -0,0 +1,9 @@
+f = open("x-files.txt", "a")
+while True:
+    line = input("give me some text to put into the file:")
+    if line.lower()=="stop":
+        break
+    f.write(line + "\n")
+
+#we need to close the file at the end
+f.close()
